39c6d5386824479fd85de6bc84c4da3f6a7a6ce6
karan kiri <karan.kiri@rebelfoods.co>
2020-05-02 11:37:55 +0530

    snake move

--- src/js/game.js
+++ src/js/game.js
@@ -9,6 +9,7 @@ export default class game {
     this.direction = 0;
     this.block = this.generateNextPoint();
     this.interval = null;
+    document.addEventListener('keyup', (event)=>this.changeDirection(event))
   }
 
   generateNextPoint() {
@@ -17,24 +18,26 @@ export default class game {
 
   moveSnake() {
     const [x, y] = this.head;
-    console.log("game -> moveSnake -> this.head", this.head)
+    //console.log("game -> moveSnake -> this.head", this.head)
+    console.log("game -> moveSnake -> this.direction", this.direction)
     switch (this.direction) {
       case 0:
         this.head = [x-1, y]
+        break;
       case 1:
         this.head = [x, y+1]
+        break;
       case 2:
-        this.head = [x-1, y]
+        this.head = [x+1, y]
+        break;
       case 3:
         this.head = [x, y-1]
+        break;
       default:
         console.log("game -> moveSnake -> this.head before render", this.head)
-        this.renderSnake()
-        if(x < 0 || y < 0 || x >= this.boxLength || y >= this.boxLength) {
-          this.resetGame()
-        }
         break;
     }
+    this.renderSnake()
   }
 
   startGame() {
@@ -49,18 +52,45 @@ export default class game {
   }
 
   renderSnake() {
-    const boxes = document.querySelectorAll(`#${this.gameContainer} .box`);
-    boxes[(this.head[0]+this.head[1])* this.boxLength].classList.add("snake");
+    const [x,y] = this.head;
+    if(x < 0 || y < 0 || x >= this.boxLength || y >= this.boxLength) {
+      return this.resetGame()
+    }
+    const boxes = [...document.querySelectorAll(`#${this.gameContainer} .box`)];
+    boxes.map((box)=> box.classList.remove('snake'));
+    boxes[(this.head[0] * this.boxLength + this.head[1])].classList.add("snake");
   }
 
   renderBlock() {
     const boxes = document.querySelectorAll(`#${this.gameContainer} .box`);
-    boxes[(this.block[0]+this.block[1])*this.boxLength].classList.add("block");
+    boxes[(this.block[0] * this.boxLength +this.block[1])].classList.add("block");
   }
 
   resetGame() {
+    console.log("game -> resetGame -> Game Over",)
     clearInterval(this.interval)
     alert("Game Over")
-    console.log("game -> resetGame -> Game Over",)
   }
+
+  changeDirection(event) {
+    console.log(event.key)
+    switch (event.key) {
+      case "ArrowUp":
+        this.direction = 0
+        break;
+      case "ArrowDown":
+        this.direction = 2
+        break;
+      case "ArrowLeft":
+        this.direction = 3
+        break;
+      case "ArrowRight":
+        this.direction = 1
+        break;
+      default:
+        break;
+    }
+    console.log("game -> changeDirection -> this.direction", this.direction)
+  }
+      
 }
